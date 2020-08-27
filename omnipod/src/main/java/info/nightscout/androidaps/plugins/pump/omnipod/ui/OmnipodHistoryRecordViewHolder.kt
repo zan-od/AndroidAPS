andyrozman/AndroidAPS-Old

Replace Omnipod history view with an infinitely scrolling version
This replaces the current Omnipod history view, which is limited to 24 hours of
data, with a new version that allows for infinite scrolling. It also highlights
pod change events.

Currently, it's still missing the event type filtering functionality of the old
view.

omnipod/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dialogs/OmnipodHistoryRecordViewHolder.kt
@@ -1,0 +1,115 @@
+package info.nightscout.androidaps.plugins.pump.omnipod.dialogs
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import info.nightscout.androidaps.data.Profile
+import info.nightscout.androidaps.db.OmnipodHistoryRecord
+import info.nightscout.androidaps.logging.AAPSLogger
+import info.nightscout.androidaps.logging.LTag
+import info.nightscout.androidaps.plugins.pump.common.data.TempBasalPair
+import info.nightscout.androidaps.plugins.pump.common.defs.PumpType
+import info.nightscout.androidaps.plugins.pump.common.utils.ProfileUtil
+import info.nightscout.androidaps.plugins.pump.omnipod.R
+import info.nightscout.androidaps.plugins.pump.omnipod.driver.db.PodHistoryEntryType
+import info.nightscout.androidaps.plugins.pump.omnipod.util.OmnipodUtil
+import info.nightscout.androidaps.utils.resources.ResourceHelper
+
+class OmnipodHistoryRecordViewHolder(view: View, val aapsLogger: AAPSLogger, val omnipodUtil: OmnipodUtil, val resourceHelper: ResourceHelper) : RecyclerView.ViewHolder(view) {
+
+    private val time: TextView = view.findViewById(R.id.omnipod_history_time)
+    private val type: TextView = view.findViewById(R.id.omnipod_history_source)
+    private val value: TextView = view.findViewById(R.id.omnipod_history_description)
+    private val layout: LinearLayout = view.findViewById(R.id.omnipod_history_item_layout)
+    private val standardBackgroundColorFilter = view.background.colorFilter
+
+    private var record: OmnipodHistoryRecord? = null
+
+    fun bind(record: OmnipodHistoryRecord?) {
+        this.record = record
+        record?.let {
+            time.text = record.dateTimeString
+            type.setText(PodHistoryEntryType.getByCode(record.podEntryTypeCode).resourceId)
+            value.text = description(record)
+
+            if (PodHistoryEntryType.getByCode(record.podEntryTypeCode) == PodHistoryEntryType.PairAndPrime) {
+                layout.background.setColorFilter(Color.parseColor("#8000ff00"), PorterDuff.Mode.OVERLAY)
+            } else {
+                layout.background.colorFilter = standardBackgroundColorFilter
+            }
+        }
+    }
+
+    private fun description(record: OmnipodHistoryRecord) : String {
+        if (record.isSuccess) {
+            return when (PodHistoryEntryType.getByCode(record.podEntryTypeCode)) {
+                PodHistoryEntryType.SetTemporaryBasal -> {
+                    val tempBasalPair: TempBasalPair = omnipodUtil.gsonInstance.fromJson(record.data, TempBasalPair::class.java)
+                    resourceHelper.gs(R.string.omnipod_cmd_tbr_value, tempBasalPair.insulinRate, tempBasalPair.durationMinutes)
+                }
+
+                PodHistoryEntryType.FillCannulaSetBasalProfile,
+                PodHistoryEntryType.SetBasalSchedule  -> profileValue(record.data)
+
+                PodHistoryEntryType.SetBolus          -> {
+                    if (record.data.contains(";")) {
+                        val splitVal = record.data.split(";".toRegex()).toTypedArray()
+                        resourceHelper.gs(R.string.omnipod_cmd_bolus_value_with_carbs, java.lang.Double.valueOf(splitVal[0]), java.lang.Double.valueOf(splitVal[1]))
+                    } else {
+                        resourceHelper.gs(R.string.omnipod_cmd_bolus_value, java.lang.Double.valueOf(record.data))
+                    }
+                }
+
+                PodHistoryEntryType.PairAndPrime -> {
+                    resourceHelper.gs(R.string.omnipod_history_new_pod_serial,record.podSerial)
+                }
+
+                PodHistoryEntryType.GetPodStatus,
+                PodHistoryEntryType.GetPodInfo,
+                PodHistoryEntryType.SetTime,
+                PodHistoryEntryType.CancelTemporaryBasal,
+                PodHistoryEntryType.CancelTemporaryBasalForce,
+                PodHistoryEntryType.ConfigureAlerts,
+                PodHistoryEntryType.CancelBolus,
+                PodHistoryEntryType.DeactivatePod,
+                PodHistoryEntryType.ResetPodState,
+                PodHistoryEntryType.AcknowledgeAlerts,
+                PodHistoryEntryType.SuspendDelivery,
+                PodHistoryEntryType.ResumeDelivery,
+                PodHistoryEntryType.UnknownEntryType  -> ""
+                else -> ""
+            }
+        } else {
+            return record.data
+        }
+    }
+
+    private fun profileValue(data: String?) : String {
+        if (data != null) {
+            aapsLogger.debug(LTag.PUMP, "Profile json:\n$data")
+            try {
+                val profileValuesArray: Array<Profile.ProfileValue> = omnipodUtil.getGsonInstance().fromJson(data, Array<Profile.ProfileValue>::class.java)
+                return ProfileUtil.getBasalProfilesDisplayable(profileValuesArray, PumpType.Insulet_Omnipod)
+            } catch (e: Exception) {
+                aapsLogger.error(LTag.PUMP, "Problem parsing Profile json. Ex: {}, Data:\n{}", e.message, data)
+                return ""
+            }
+        } else {
+            return ""
+        }
+    }
+
+    companion object {
+
+        fun create(parent: ViewGroup, aapsLogger: AAPSLogger, omnipodUtil: OmnipodUtil, resourceHelper: ResourceHelper) : OmnipodHistoryRecordViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.omnipod_pod_history_item, parent, false)
+            return OmnipodHistoryRecordViewHolder(view, aapsLogger, omnipodUtil, resourceHelper)
+        }
+    }
+
+}

omnipod/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dialogs/OmnipodHistoryRecordsAdapter.kt
@@ -1,0 +1,41 @@
+package info.nightscout.androidaps.plugins.pump.omnipod.dialogs
+
+import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import info.nightscout.androidaps.db.OmnipodHistoryRecord
+import info.nightscout.androidaps.logging.AAPSLogger
+import info.nightscout.androidaps.plugins.pump.omnipod.util.OmnipodUtil
+import info.nightscout.androidaps.utils.resources.ResourceHelper
+import javax.inject.Inject
+
+class OmnipodHistoryRecordsAdapter @Inject constructor() : PagedListAdapter<OmnipodHistoryRecord, OmnipodHistoryRecordViewHolder>(DIFF_CALLBACK) {
+
+    @Inject lateinit var aapsLogger: AAPSLogger
+    @Inject lateinit var omnipodUtil: OmnipodUtil
+    @Inject lateinit var resourceHelper: ResourceHelper
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OmnipodHistoryRecordViewHolder {
+        return OmnipodHistoryRecordViewHolder.create(parent ,aapsLogger, omnipodUtil, resourceHelper)
+    }
+
+    override fun onBindViewHolder(holder: OmnipodHistoryRecordViewHolder, position: Int) {
+        val record: OmnipodHistoryRecord? = getItem(position)
+        holder.bind(record)
+    }
+
+    companion object {
+
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<OmnipodHistoryRecord>() {
+
+            override fun areItemsTheSame(oldItem: OmnipodHistoryRecord, newItem: OmnipodHistoryRecord) =
+                oldItem.compareTo(newItem) == 0
+
+            override fun areContentsTheSame(oldItem: OmnipodHistoryRecord, newItem: OmnipodHistoryRecord) =
+                areItemsTheSame(oldItem,newItem)
+
+        }
+
+    }
+
+}

omnipod/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dialogs/PodHistoryActivity.kt
@@ -1,0 +1,92 @@
+package info.nightscout.androidaps.plugins.pump.omnipod.dialogs
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.paging.DataSource
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import info.nightscout.androidaps.activities.NoSplashAppCompatActivity
+import info.nightscout.androidaps.db.OmnipodHistoryRecord
+import info.nightscout.androidaps.interfaces.DatabaseHelperInterface
+import info.nightscout.androidaps.logging.AAPSLogger
+import info.nightscout.androidaps.plugins.pump.common.defs.PumpHistoryEntryGroup
+import info.nightscout.androidaps.plugins.pump.omnipod.data.OmnipodHistoryRecordDataSource
+import info.nightscout.androidaps.plugins.pump.omnipod.defs.state.PodStateManager
+import info.nightscout.androidaps.plugins.pump.omnipod.util.OmnipodUtil
+import info.nightscout.androidaps.plugins.pump.omnipod.R
+import info.nightscout.androidaps.utils.resources.ResourceHelper
+import javax.inject.Inject
+
+class PodHistoryActivity : NoSplashAppCompatActivity() {
+
+    @Inject lateinit var aapsLogger: AAPSLogger
+    @Inject lateinit var omnipodUtil: OmnipodUtil
+    @Inject lateinit var resourceHelper: ResourceHelper
+    @Inject lateinit var databaseHelper: DatabaseHelperInterface
+    @Inject lateinit var podStateManager: PodStateManager
+    @Inject lateinit var omnipodHistoryRecordsAdapter: OmnipodHistoryRecordsAdapter
+
+    private var historyTypeSpinner: Spinner? = null
+    //private var statusView: TextView? = null
+    //private var recyclerView: RecyclerView? = null
+    //private var linearLayoutManager: LinearLayoutManager? = null
+
+    var showingType: OldPodHistoryActivity.TypeList? = null
+    var selectedGroup = PumpHistoryEntryGroup.All
+    var fullHistoryList: List<OmnipodHistoryRecord> = ArrayList()
+    var filteredHistoryList: List<OmnipodHistoryRecord> = ArrayList()
+
+    var manualChange = false
+
+    var typeListFull: List<OldPodHistoryActivity.TypeList>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.omnipod_pod_history_activity)
+        historyTypeSpinner = findViewById(R.id.omnipod_historytype)
+        val statusView : TextView = findViewById(R.id.omnipod_historystatus)
+        val recyclerView : RecyclerView = findViewById(R.id.omnipod_history_recyclerview)
+        recyclerView.setHasFixedSize(true)
+        //linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        //recyclerViewAdapter = OldPodHistoryActivity.RecyclerViewAdapter(filteredHistoryList)
+        recyclerView.adapter = omnipodHistoryRecordsAdapter
+        recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context,DividerItemDecoration.VERTICAL))
+        statusView.visibility = View.GONE
+
+        val dataSourceFactory = object : DataSource.Factory<Long,OmnipodHistoryRecord>() {
+            override fun create(): DataSource<Long, OmnipodHistoryRecord> {
+                return OmnipodHistoryRecordDataSource(databaseHelper,aapsLogger)
+            }
+        }
+
+        val records = LivePagedListBuilder(dataSourceFactory, PagedList.Config.Builder().setPageSize(30).build()).build()
+
+        records.observe(this, Observer<PagedList<OmnipodHistoryRecord>>{ pagedList -> omnipodHistoryRecordsAdapter.submitList(pagedList) } )
+
+        // typeListFull = getTypeList(PumpHistoryEntryGroup.getTranslatedList(resourceHelper))
+        // // val spinnerAdapter = ArrayAdapter(this, R.layout.spinner_centered, typeListFull)
+        // historyTypeSpinner.setAdapter(spinnerAdapter)
+        // historyTypeSpinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+        //     override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+        //         if (manualChange) return
+        //         val selected = historyTypeSpinner.getSelectedItem() as OldPodHistoryActivity.TypeList
+        //         OldPodHistoryActivity.showingType = selected
+        //         OldPodHistoryActivity.selectedGroup = selected.entryGroup
+        //         filterHistory(OldPodHistoryActivity.selectedGroup)
+        //     }
+        //
+        //     override fun onNothingSelected(parent: AdapterView<*>?) {
+        //         if (manualChange) return
+        //         filterHistory(PumpHistoryEntryGroup.All)
+        //     }
+        // })
+    }
+
+}

zan-od/AndroidAPS

omnipod/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/ui/OmnipodHistoryRecordViewHolder.kt
@@ -1,0 +1,115 @@
+package info.nightscout.androidaps.plugins.pump.omnipod.ui
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import info.nightscout.androidaps.data.Profile
+import info.nightscout.androidaps.db.OmnipodHistoryRecord
+import info.nightscout.androidaps.logging.AAPSLogger
+import info.nightscout.androidaps.logging.LTag
+import info.nightscout.androidaps.plugins.pump.common.data.TempBasalPair
+import info.nightscout.androidaps.plugins.pump.common.defs.PumpType
+import info.nightscout.androidaps.plugins.pump.common.utils.ProfileUtil
+import info.nightscout.androidaps.plugins.pump.omnipod.R
+import info.nightscout.androidaps.plugins.pump.omnipod.definition.PodHistoryEntryType
+import info.nightscout.androidaps.plugins.pump.omnipod.util.AapsOmnipodUtil
+import info.nightscout.androidaps.utils.resources.ResourceHelper
+
+class OmnipodHistoryRecordViewHolder(view: View, val aapsLogger: AAPSLogger, val aapsOmnipodUtil: AapsOmnipodUtil, val resourceHelper: ResourceHelper) : RecyclerView.ViewHolder(view) {
+
+    private val time: TextView = view.findViewById(R.id.omnipod_history_time)
+    private val type: TextView = view.findViewById(R.id.omnipod_history_source)
+    private val value: TextView = view.findViewById(R.id.omnipod_history_description)
+    private val layout: LinearLayout = view.findViewById(R.id.omnipod_history_item_layout)
+    private val standardBackgroundColorFilter = view.background.colorFilter
+
+    private var record: OmnipodHistoryRecord? = null
+
+    fun bind(record: OmnipodHistoryRecord?) {
+        this.record = record
+        record?.let {
+            time.text = record.dateTimeString
+            type.setText(PodHistoryEntryType.getByCode(record.podEntryTypeCode).resourceId)
+            value.text = description(record)
+
+            if (PodHistoryEntryType.getByCode(record.podEntryTypeCode) == PodHistoryEntryType.PairAndPrime) {
+                layout.background.setColorFilter(Color.parseColor("#8000ff00"), PorterDuff.Mode.OVERLAY)
+            } else {
+                layout.background.colorFilter = standardBackgroundColorFilter
+            }
+        }
+    }
+
+    private fun description(record: OmnipodHistoryRecord) : String {
+        if (record.isSuccess) {
+            return when (PodHistoryEntryType.getByCode(record.podEntryTypeCode)) {
+                PodHistoryEntryType.SetTemporaryBasal -> {
+                    val tempBasalPair: TempBasalPair = aapsOmnipodUtil.gsonInstance.fromJson(record.data, TempBasalPair::class.java)
+                    resourceHelper.gs(R.string.omnipod_cmd_tbr_value, tempBasalPair.insulinRate, tempBasalPair.durationMinutes)
+                }
+
+                PodHistoryEntryType.FillCannulaSetBasalProfile,
+                PodHistoryEntryType.SetBasalSchedule  -> profileValue(record.data)
+
+                PodHistoryEntryType.SetBolus          -> {
+                    if (record.data.contains(";")) {
+                        val splitVal = record.data.split(";".toRegex()).toTypedArray()
+                        resourceHelper.gs(R.string.omnipod_cmd_bolus_value_with_carbs, java.lang.Double.valueOf(splitVal[0]), java.lang.Double.valueOf(splitVal[1]))
+                    } else {
+                        resourceHelper.gs(R.string.omnipod_cmd_bolus_value, java.lang.Double.valueOf(record.data))
+                    }
+                }
+
+                PodHistoryEntryType.PairAndPrime -> {
+                    resourceHelper.gs(R.string.omnipod_history_new_pod_serial,record.podSerial)
+                }
+
+                PodHistoryEntryType.GetPodStatus,
+                PodHistoryEntryType.GetPodInfo,
+                PodHistoryEntryType.SetTime,
+                PodHistoryEntryType.CancelTemporaryBasal,
+                PodHistoryEntryType.CancelTemporaryBasalForce,
+                PodHistoryEntryType.ConfigureAlerts,
+                PodHistoryEntryType.CancelBolus,
+                PodHistoryEntryType.DeactivatePod,
+                PodHistoryEntryType.ResetPodState,
+                PodHistoryEntryType.AcknowledgeAlerts,
+                PodHistoryEntryType.SuspendDelivery,
+                PodHistoryEntryType.ResumeDelivery,
+                PodHistoryEntryType.UnknownEntryType  -> ""
+                else -> ""
+            }
+        } else {
+            return record.data
+        }
+    }
+
+    private fun profileValue(data: String?) : String {
+        if (data != null) {
+            aapsLogger.debug(LTag.PUMP, "Profile json:\n$data")
+            try {
+                val profileValuesArray: Array<Profile.ProfileValue> = aapsOmnipodUtil.getGsonInstance().fromJson(data, Array<Profile.ProfileValue>::class.java)
+                return ProfileUtil.getBasalProfilesDisplayable(profileValuesArray, PumpType.Insulet_Omnipod)
+            } catch (e: Exception) {
+                aapsLogger.error(LTag.PUMP, "Problem parsing Profile json. Ex: {}, Data:\n{}", e.message, data)
+                return ""
+            }
+        } else {
+            return ""
+        }
+    }
+
+    companion object {
+
+        fun create(parent: ViewGroup, aapsLogger: AAPSLogger, aapsOmnipodUtil: AapsOmnipodUtil, resourceHelper: ResourceHelper) : OmnipodHistoryRecordViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.omnipod_pod_history_item, parent, false)
+            return OmnipodHistoryRecordViewHolder(view, aapsLogger, aapsOmnipodUtil, resourceHelper)
+        }
+    }
+
+}

omnipod/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/ui/OmnipodHistoryRecordsAdapter.kt
@@ -1,0 +1,41 @@
+package info.nightscout.androidaps.plugins.pump.omnipod.ui
+
+import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import info.nightscout.androidaps.db.OmnipodHistoryRecord
+import info.nightscout.androidaps.logging.AAPSLogger
+import info.nightscout.androidaps.plugins.pump.omnipod.util.AapsOmnipodUtil
+import info.nightscout.androidaps.utils.resources.ResourceHelper
+import javax.inject.Inject
+
+class OmnipodHistoryRecordsAdapter @Inject constructor() : PagedListAdapter<OmnipodHistoryRecord, OmnipodHistoryRecordViewHolder>(DIFF_CALLBACK) {
+
+    @Inject lateinit var aapsLogger: AAPSLogger
+    @Inject lateinit var resourceHelper: ResourceHelper
+    @Inject lateinit var aapsOmnipodUtil: AapsOmnipodUtil
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OmnipodHistoryRecordViewHolder {
+        return OmnipodHistoryRecordViewHolder.create(parent ,aapsLogger, aapsOmnipodUtil, resourceHelper)
+    }
+
+    override fun onBindViewHolder(holder: OmnipodHistoryRecordViewHolder, position: Int) {
+        val record: OmnipodHistoryRecord? = getItem(position)
+        holder.bind(record)
+    }
+
+    companion object {
+
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<OmnipodHistoryRecord>() {
+
+            override fun areItemsTheSame(oldItem: OmnipodHistoryRecord, newItem: OmnipodHistoryRecord) =
+                oldItem.compareTo(newItem) == 0
+
+            override fun areContentsTheSame(oldItem: OmnipodHistoryRecord, newItem: OmnipodHistoryRecord) =
+                areItemsTheSame(oldItem,newItem)
+
+        }
+
+    }
+
+}

omnipod/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/ui/PodHistoryActivity.kt
@@ -1,0 +1,90 @@
+package info.nightscout.androidaps.plugins.pump.omnipod.ui
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.paging.DataSource
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import info.nightscout.androidaps.activities.NoSplashAppCompatActivity
+import info.nightscout.androidaps.db.OmnipodHistoryRecord
+import info.nightscout.androidaps.interfaces.DatabaseHelperInterface
+import info.nightscout.androidaps.logging.AAPSLogger
+import info.nightscout.androidaps.plugins.pump.common.defs.PumpHistoryEntryGroup
+import info.nightscout.androidaps.plugins.pump.omnipod.data.OmnipodHistoryRecordDataSource
+import info.nightscout.androidaps.plugins.pump.omnipod.manager.AapsPodStateManager
+import info.nightscout.androidaps.plugins.pump.omnipod.R
+import info.nightscout.androidaps.utils.resources.ResourceHelper
+import javax.inject.Inject
+
+class PodHistoryActivity : NoSplashAppCompatActivity() {
+
+    @Inject lateinit var aapsLogger: AAPSLogger
+    @Inject lateinit var resourceHelper: ResourceHelper
+    @Inject lateinit var databaseHelper: DatabaseHelperInterface
+    @Inject lateinit var podStateManager: AapsPodStateManager
+    @Inject lateinit var omnipodHistoryRecordsAdapter: OmnipodHistoryRecordsAdapter
+
+    private var historyTypeSpinner: Spinner? = null
+    //private var statusView: TextView? = null
+    //private var recyclerView: RecyclerView? = null
+    //private var linearLayoutManager: LinearLayoutManager? = null
+
+    //var showingType: OldPodHistoryActivity.TypeList? = null
+    var selectedGroup = PumpHistoryEntryGroup.All
+    var fullHistoryList: List<OmnipodHistoryRecord> = ArrayList()
+    var filteredHistoryList: List<OmnipodHistoryRecord> = ArrayList()
+
+    var manualChange = false
+
+    //var typeListFull: List<OldPodHistoryActivity.TypeList>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.omnipod_pod_history_activity)
+        historyTypeSpinner = findViewById(R.id.omnipod_historytype)
+        val statusView : TextView = findViewById(R.id.omnipod_historystatus)
+        val recyclerView : RecyclerView = findViewById(R.id.omnipod_history_recyclerview)
+        recyclerView.setHasFixedSize(true)
+        //linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        //recyclerViewAdapter = OldPodHistoryActivity.RecyclerViewAdapter(filteredHistoryList)
+        recyclerView.adapter = omnipodHistoryRecordsAdapter
+        recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context,DividerItemDecoration.VERTICAL))
+        statusView.visibility = View.GONE
+
+        val dataSourceFactory = object : DataSource.Factory<Long,OmnipodHistoryRecord>() {
+            override fun create(): DataSource<Long, OmnipodHistoryRecord> {
+                return OmnipodHistoryRecordDataSource(databaseHelper,aapsLogger)
+            }
+        }
+
+        val records = LivePagedListBuilder(dataSourceFactory, PagedList.Config.Builder().setPageSize(30).build()).build()
+
+        records.observe(this, Observer<PagedList<OmnipodHistoryRecord>>{ pagedList -> omnipodHistoryRecordsAdapter.submitList(pagedList) } )
+
+        // typeListFull = getTypeList(PumpHistoryEntryGroup.getTranslatedList(resourceHelper))
+        // // val spinnerAdapter = ArrayAdapter(this, R.layout.spinner_centered, typeListFull)
+        // historyTypeSpinner.setAdapter(spinnerAdapter)
+        // historyTypeSpinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+        //     override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+        //         if (manualChange) return
+        //         val selected = historyTypeSpinner.getSelectedItem() as OldPodHistoryActivity.TypeList
+        //         OldPodHistoryActivity.showingType = selected
+        //         OldPodHistoryActivity.selectedGroup = selected.entryGroup
+        //         filterHistory(OldPodHistoryActivity.selectedGroup)
+        //     }
+        //
+        //     override fun onNothingSelected(parent: AdapterView<*>?) {
+        //         if (manualChange) return
+        //         filterHistory(PumpHistoryEntryGroup.All)
+        //     }
+        // })
+    }
+
+}