a85ab561bdb06cbe352634266fa077763a4e9e47
Robert <comps@diprobe.com>
2021-05-18 13:10:52 +0200

    pod & bolus button

--- app/src/main/java/info/nightscout/androidaps/skins/SkinClassic.kt
+++ app/src/main/java/info/nightscout/androidaps/skins/SkinClassic.kt
@@ -9,7 +9,7 @@ import javax.inject.Singleton
 class SkinClassic @Inject constructor(private val config: Config): SkinInterface {
 
     override val description: Int get() = R.string.classic_description
-    override val mainGraphHeight: Int get() = 270
+    override val mainGraphHeight: Int get() = 280
     override val secondaryGraphHeight: Int get() = 210
 
     override fun overviewLayout(isLandscape: Boolean, isTablet: Boolean, isSmallHeight: Boolean): Int =
