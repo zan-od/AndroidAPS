43e0f82501556b0dd8d249164aefee2ce6f21852
Kirill Chekanov <chekanov.kirill@gmail.com>
2021-09-12 18:23:32 +0300

    Engineering mode checkbox

--- app/src/main/java/info/nightscout/androidaps/utils/buildHelper/BuildHelperImpl.kt
+++ app/src/main/java/info/nightscout/androidaps/utils/buildHelper/BuildHelperImpl.kt
@@ -2,12 +2,15 @@ package info.nightscout.androidaps.utils.buildHelper
 
 import info.nightscout.androidaps.BuildConfig
 import info.nightscout.androidaps.interfaces.Config
+import info.nightscout.androidaps.R
 import info.nightscout.androidaps.plugins.general.maintenance.PrefFileListProvider
+import info.nightscout.androidaps.utils.sharedPreferences.SP
 import java.io.File
 
 class BuildHelperImpl constructor(
     private val config: Config,
-    fileListProvider: PrefFileListProvider
+    fileListProvider: PrefFileListProvider,
+    private val sp: SP
 ) : BuildHelper {
 
     private var devBranch = false
@@ -16,7 +19,7 @@ class BuildHelperImpl constructor(
     init {
         val engineeringModeSemaphore = File(fileListProvider.ensureExtraDirExists(), "engineering__mode")
 
-        engineeringMode = engineeringModeSemaphore.exists() && engineeringModeSemaphore.isFile
+        engineeringMode = engineeringModeSemaphore.exists() && engineeringModeSemaphore.isFile || sp.getBoolean(R.string.key_engineering_mode, false)
         devBranch = BuildConfig.VERSION.contains("-") || BuildConfig.VERSION.matches(Regex(".*[a-zA-Z]+.*"))
     }
 
